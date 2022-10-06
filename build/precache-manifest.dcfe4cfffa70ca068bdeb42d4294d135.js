fb5436bc6bcba82a1d0637040947107c89d4b8e2
Raiyan Rofiquzzaman <raiyanrofiquzzaman@hotmail.com>
2022-10-06 13:02:30 -0400

    React project code added

---
+++ build/precache-manifest.dcfe4cfffa70ca068bdeb42d4294d135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1da997a0d4b415e7f8da",
+    "url": "/612943810/COMP3104_Lab2/static/css/main.f3de8a43.chunk.css"
+  },
+  {
+    "revision": "1da997a0d4b415e7f8da",
+    "url": "/612943810/COMP3104_Lab2/static/js/main.1da997a0.chunk.js"
+  },
+  {
+    "revision": "0ef31d01219edde20090",
+    "url": "/612943810/COMP3104_Lab2/static/js/runtime~main.0ef31d01.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/612943810/COMP3104_Lab2/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "8e7639b5002b42e1995be9da947b0373",
+    "url": "/612943810/COMP3104_Lab2/index.html"
+  }
+];
